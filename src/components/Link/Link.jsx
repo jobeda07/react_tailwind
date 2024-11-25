e3b3eccacf367734e47164a7fcf186b6b7701050
jobeda <jobedakhatun710@gmail.com>
2024-11-25 12:22:35 +0600

    nav toggle

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 const Link = ({route}) => {
@@ -7,5 +8,7 @@ const Link = ({route}) => {
  );
 };
 
-
+Link.propTypes={
+ route:PropTypes.object
+}
 export default Link;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { RiMenu2Fill } from "react-icons/ri";
+import { MdOutlineClose } from "react-icons/md";
+
 
 const Navbar = () => {
- 
-  const routes =[
-      {id:1,path:'/',name:'Home'},
-      {id:2,path:'/about',name:'About'},
-      {id:3,path:'/services',name:'Services'},
-      {id:4,path:'/contact',name:'Contact'},
-      {id:5,path:'*',name:'NotFound'},
-  ];
+ const [open, setOpen] = useState(false)
+
+ const routes = [
+  { id: 1, path: '/', name: 'Home' },
+  { id: 2, path: '/about', name: 'About' },
+  { id: 3, path: '/services', name: 'Services' },
+  { id: 4, path: '/contact', name: 'Contact' },
+  { id: 5, path: '*', name: 'NotFound' },
+ ];
 
  return (
   <nav>
-    <ul className='md:flex'>
+   <div onClick={() => setOpen(!open)} className='md:hidden text-3xl'>
     {
-     routes.map(route=><Link key={route.id} route={route}></Link>)
+     open===true?  <RiMenu2Fill></RiMenu2Fill>:<MdOutlineClose></MdOutlineClose>
     }
-    </ul>
+    
    
+   </div>
+
+   <ul className='md:flex'>
+    {
+     routes.map(route => <Link key={route.id} route={route}></Link>)
+    }
+   </ul>
+
   </nav>
  );
 };
