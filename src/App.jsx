11ed86e77aebf1590701b2aad048dd686c24137d
jobeda <jobedakhatun710@gmail.com>
2024-11-25 11:28:11 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar></Navbar>
       <div>
         <h1 className='text-center'>hi picchi</h1>
       </div>
